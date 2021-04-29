b0d8a1cb7658eabfeb9f3d53757ce7afac336a98
Andrenerd <andreid.31415@gmail.com>
2021-04-29 04:56:39 +0200

    update blobbercore pending tests

--- code/go/0chain.net/blobbercore/allocation/entity_test.go
+++ code/go/0chain.net/blobbercore/allocation/entity_test.go
@@ -2,6 +2,7 @@ package allocation
 
 import (
 	"testing"
+	// "gorm.io/gorm"
 	"github.com/stretchr/testify/assert"
 
 	"0chain.net/core/common"
@@ -66,8 +67,8 @@ func TestAllocationWantWrite(t *testing.T) {
 		want int64
 	}{
 		{
-			name: "Table name",
-			args: args{blobberID: "0",size: 0, wmt: common.Timestamp(0)}, 
+			name: "WantWrite / Basic",
+			args: args{blobberID: "0", size: 0, wmt: common.Timestamp(0)},
 			want: 0,
 		},
 	}
@@ -79,6 +80,105 @@ func TestAllocationWantWrite(t *testing.T) {
 			got := allocation.WantWrite(
 				tt.args.blobberID, tt.args.size, tt.args.wmt);
 			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestAllocationHaveRead(t *testing.T) {
+	type args struct {
+		blobberID string
+		pendNumBlocks int64
+		readPools []*ReadPool
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			name: "HaveRead / Basic",
+			args: args{
+				blobberID: "0",
+				pendNumBlocks: 16384,
+				readPools: []*ReadPool{
+					&ReadPool{Balance: 1},
+					&ReadPool{Balance: 2},
+				},
+			},
+			want: 3,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			allocation := Allocation{}
+
+			got := allocation.HaveRead(
+				tt.args.readPools, tt.args.blobberID, tt.args.pendNumBlocks);
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestPendingAddPendingWrite(t *testing.T) {
+	type args struct {
+		size int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			name: "AddPendingWrite / Basic",
+			args: args{size: 1},
+			want: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pending := Pending{}
+
+			pending.AddPendingWrite(tt.args.size);
+			assert.Equal(t, tt.want, pending.PendingWrite)
+		})
+	}
+}
+
+func TestPendingSubPendingWrite(t *testing.T) {
+	type fields struct {
+		size int64
+	}
+	type args struct {
+		size int64
+	}
+	tests := []struct {
+		name string
+		args args
+		fields fields
+		want int64
+	}{
+		{
+			name: "SubPendingWrite / Basic",
+			args: args{size: 1},
+			fields: fields{size: 2},
+			want: 1,
+		},
+		{
+			name: "SubPendingWrite / Below zero",
+			args: args{size: 2},
+			fields: fields{size: 1},
+			want: 0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pending := Pending{PendingWrite: tt.fields.size}
+
+			pending.SubPendingWrite(tt.args.size);
+			assert.Equal(t, tt.want, pending.PendingWrite)
 		})
 	}
 }
